d499876fa02efc05ac9c8382603991c1f64f3104
Fangyan SUN <zhzxsunfangyan@yeah.net>
2019-06-20 16:12:00 +0200

    --add image for each film via API --add functionalities like "Waiting page" etc
    Signed-off-by: Fangyan SUN <zhzxsunfangyan@yeah.net>

--- Components/Search.js
+++ Components/Search.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {StyleSheet, View, TextInput, FlatList, Text, Button} from 'react-native'
+import {StyleSheet, View, TextInput, FlatList, Text, Button, ActivityIndicator} from 'react-native'
 import FilmItem from '../Components/FilmItem'
 import {getFilmsFromApiWithSearchedText} from '../API/TMDBApi'
 
@@ -11,15 +11,34 @@ class Search extends React.Component{
 	constructor(props){
 		super(props)
 		this.searchedText= ""
-		this.state = {films: []}
+		this.state = {
+			films: [],
+			isLoading: false
+		}
 	}
 
-	_loadFilms(){
-		if (this.searchedText.length>0){
-			getFilmsFromApiWithSearchedText(this.searchedText).then(data => this.setState({films: data.results}));
-		}
-		
+	_loadFilms() {
+	    if (this.searchedText.length > 0) {
+	      this.setState({ isLoading: true }) // Lancement du chargement
+	      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+	          this.setState({ 
+	            films: data.results,
+	            isLoading: false
+	          })
+	      })
+	    }
 	}
+
+	_displayLoading() {
+      if (this.state.isLoading) {
+        return (
+          <View style={styles.loading_container}>
+            <ActivityIndicator size='large' />
+          </View>
+        )
+      }
+    }
+
 
 	_searchTextInputChanged(text){
 		this.searchedText = text
@@ -27,21 +46,24 @@ class Search extends React.Component{
 
 
 	render(){
+		console.log(this.state.isLoading)
 		return (
 			<View style={styles.main_container}>
         		<TextInput 
         			style={styles.textinput} 
+        			onSubmitEditing={()=> this._loadFilms}
         			placeholder='Titre du film'
         			onChangeText={(text) => this._searchTextInputChanged(text)}
         		/>
 
-            	<Button style={styles.button} title='Rechercher' color="#841584" onPress={() => this._loadFilms()}/>
+            	<Button title='Rechercher' color="#841584" onPress={() => this._loadFilms()}/>
 
 	            <FlatList
 					data={this.state.films}
 					keyExtractor={(item) => item.id.toString()}
 					renderItem={({item}) => <FilmItem film={item}/>}
 				/>
+				{this._displayLoading()}
       		</View>
 		)
 	}
@@ -63,12 +85,15 @@ const styles = StyleSheet.create({
     	paddingLeft: 5
   	},
 
-  	button:{
-  		height:10,
-  		margin:5,
-  		padding:5,
-  		flex:1, 
-  	}
+  	loading_container: {
+	    position: 'absolute',
+	    left: 0,
+	    right: 0,
+	    top: 100,
+	    bottom: 0,
+	    alignItems: 'center',
+	    justifyContent: 'center'
+	}
 });
 
 
